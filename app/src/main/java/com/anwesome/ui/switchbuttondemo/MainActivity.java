b252f9e3035a7a84e10b73ca0756d83c68f41eaf
Anwesh43 <anweshthecool0@gmail.com>
2017-02-02 03:08:50 +0530

    Implementing the switchbutton in MainActivity

--- app/src/main/java/com/anwesome/ui/switchbuttondemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/switchbuttondemo/MainActivity.java
@@ -4,11 +4,18 @@ import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.anwesome.ui.switchbutton.SwitchButtonGroup;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SwitchButtonGroup switchButtonGroup = new SwitchButtonGroup(this);
+        switchButtonGroup.addAction("RED");
+        switchButtonGroup.addAction("GREEN");
+        switchButtonGroup.addAction("BLUE");
+        switchButtonGroup.show(400);
     }
 }
