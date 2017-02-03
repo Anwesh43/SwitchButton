2b0b8e1b4ef272acd64db91112fc126238761892
Anwesh43 <anweshthecool0@gmail.com>
2017-02-03 09:17:13 +0530

    Implementing onSelectListener

--- app/src/main/java/com/anwesome/ui/switchbuttondemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/switchbuttondemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.anwesome.ui.switchbuttondemo;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.anwesome.ui.switchbutton.SwitchButtonGroup;
 
@@ -13,9 +14,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         SwitchButtonGroup switchButtonGroup = new SwitchButtonGroup(this);
-        switchButtonGroup.addAction("RED");
-        switchButtonGroup.addAction("GREEN");
-        switchButtonGroup.addAction("BLUE");
+        switchButtonGroup.addAction("RED", new SwitchButtonGroup.OnSelectListener() {
+            @Override
+            public void onSelect() {
+                Toast.makeText(MainActivity.this,"RED",Toast.LENGTH_SHORT).show();
+            }
+        });
+        switchButtonGroup.addAction("GREEN",new SwitchButtonGroup.OnSelectListener() {
+            @Override
+            public void onSelect() {
+                Toast.makeText(MainActivity.this, "GREEN", Toast.LENGTH_SHORT).show();
+            }
+        });
+        switchButtonGroup.addAction("BLUE",new SwitchButtonGroup.OnSelectListener() {
+                @Override
+                public void onSelect() {
+                    Toast.makeText(MainActivity.this, "BLUE", Toast.LENGTH_SHORT).show();
+                }
+        });
         switchButtonGroup.show(400);
     }
 }
